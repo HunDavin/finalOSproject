648ba5fe21fc2d3d66869e407935665398df78db
Pychey <prakpycheyy@gmail.com>
2025-04-01 22:19:53 +0700

    Modify Compress Program

--- script/compress.sh
+++ script/compress.sh
@@ -1,25 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-log_file="operation.log"
+read -p "Enter file or directory to Compress: " dir
+baseName=$(basename "$dir")
 
-log_message() {
-	echo "$(date '+%Y-%m-%d %H:%M:%S') -$1" >> "$log_file"
-}
-if [ ! -f "log_file" ]; then
-	touch "$log_file"
-	log_message "Log file created"
-fi
+[ ! -e "zip" ] && mkdir "zip"
 
-echo "Enter file or directory"
-read dir
 if [ -f "$dir" ]; then
-	zip "$dir.zip" "$dir"
-	log_message "compressed $dir"
-elif [ -d "dir" ]; then
-	zip -r "dir.zip" "$dir"
-	log_message "compressed folder $dir"
+	zip "./zip/$baseName.zip" "$dir"
+	./script/logOperation.sh "compressed $dir"
+elif [ -d "$dir" ]; then
+	zip -r "./zip/$baseName.zip" "$dir"
+	./script/logOperation.sh "compressed folder $dir"
 else 
-	echo "Error : No such file or directory"
-	log_message "error: no file or directory"
-	exit 1
+	echo "Compress Error: $dir Dont Exist"
+	./script/logOperation.sh "Compress Error: $dir Dont Exist"
 fi
